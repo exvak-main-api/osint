c9811a4ce04270ea27399f46d2a51eb984c907fe
exvak-main-api <sigmagang2025@gmail.com>
2026-05-15 01:36:33 +0100

    Update duolingo.py

--- modules/accounts/duolingo.py
+++ modules/accounts/duolingo.py
@@ -1,18 +1,26 @@
 from lib.requests import Request
-from lib.colors import *
 
 async def duolingo(target: str):
 
-    r = await Request(f"https://www.duolingo.com/2017-06-30/users", params={'email': target}).get()
-
     try:
-        if """{"users":[]}""" in r.text:
-            print(f"{RED}>{WHITE} Duolingo")
+        r = await Request(
+            "https://www.duolingo.com/2017-06-30/users",
+            params={'email': target}
+        ).get()
 
-        else:
-            valid = r.json()['users'][0]['username']
-            
-            print(f"{GREEN}>{WHITE} Duolingo")
+        if '{"users":[]}' in r.text:
+            return False
+
+        try:
+            data = r.json()
+
+            if data.get('users') and len(data['users']) > 0:
+                return True
+
+            return False
+
+        except:
+            return False
 
     except:
-        print(f"{RED}>{WHITE} Duolingo")
+        return False
