af60b152c67dec5ecc0d11447a078808a83beab0
exvak-main-api <sigmagang2025@gmail.com>
2026-05-15 01:36:13 +0100

    Update chess.py

--- modules/accounts/chess.py
+++ modules/accounts/chess.py
@@ -1,19 +1,21 @@
 from lib.requests import Request
-from lib.colors import *
 
 async def chess(target: str):
 
-    r = await Request(f"https://www.chess.com/callback/email/available?email={target}").post()
-
     try:
-        if r.json()['isEmailAvailable'] == True:
-            print(f"{RED}>{WHITE} Chess")
+        r = await Request(
+            f"https://www.chess.com/callback/email/available?email={target}"
+        ).post()
 
-        elif r.json()['isEmailAvailable'] == False:
-            print(f"{GREEN}>{WHITE} Chess")
+        try:
+            data = r.json()
+        except:
+            return False
 
-        else:
-            print(f"{RED}>{WHITE} Chess")
+        if data.get('isEmailAvailable') == False:
+            return True
+
+        return False
 
     except:
-        pass
+        return False
