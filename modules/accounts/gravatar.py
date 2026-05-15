d63edb816bb7b7051b4e26e1841dfee09a084782
exvak-main-api <sigmagang2025@gmail.com>
2026-05-15 01:39:47 +0100

    Update gravatar.py

--- modules/accounts/gravatar.py
+++ modules/accounts/gravatar.py
@@ -1,34 +1,38 @@
 from lib.requests import Request
-from lib.colors import *
 import hashlib
 
 async def gravatar(target: str):
 
-    encoded_email = target.lower().encode('utf-8')
-    hashed_email = hashlib.sha256(encoded_email).hexdigest()
-
-    r = await Request(f"https://en.gravatar.com/{hashed_email}.json").get()
-
     try:
-        if "User not found" in r.text:
-            print(f"{RED}>{WHITE} Gravatar")
+        encoded_email = target.lower().encode('utf-8')
+        hashed_email = hashlib.sha256(encoded_email).hexdigest()
 
-        else:
+        r = await Request(
+            f"https://en.gravatar.com/{hashed_email}.json"
+        ).get()
+
+        if "User not found" in r.text:
+            return False
+
+        try:
             data = r.json()['entry'][0]
 
-            print(f"{GREEN}>{WHITE} Gravatar")
-            print(f"  ├──> Username : {data['displayName']}")
+            print(f"  ├── Username : {data['displayName']}")
 
             try:
-                avatar_url_seaked = data['thumbnailUrl']
-                avatar_url = str(avatar_url_seaked).replace("\\", "")
-
-                print(f"  ├──> Avatar : {avatar_url}")
-
+                avatar_url = str(data['thumbnailUrl']).replace("\\", "")
+                print(f"  ├── Avatar : {avatar_url}")
             except:
                 pass
 
-            print(f"  └──> Account : {CYAN}https://gravatar.com/{data['displayName']}/{WHITE}")
+            print(
+                f"  └── Account : https://gravatar.com/{data['displayName']}/"
+            )
+
+            return True
+
+        except:
+            return False
 
     except:
-        print(f"{RED}>{WHITE} Gravatar")
+        return False
