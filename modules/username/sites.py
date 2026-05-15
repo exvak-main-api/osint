25cd4f44580334c04d9d2aa3a0b5e97cc7faead7
exvak-main-api <sigmagang2025@gmail.com>
2026-05-15 05:12:17 +0100

    Update sites.py

--- modules/username/sites.py
+++ modules/username/sites.py
@@ -1,1 +1,28 @@
+SITES = {
+    "github": "https://github.com/{}",
+    "reddit": "https://www.reddit.com/user/{}",
+    "twitter": "https://nitter.net/{}",
+    "instagram": "https://www.instagram.com/{}",
+    "tiktok": "https://www.tiktok.com/@{}",
+    "pinterest": "https://www.pinterest.com/{}",
+    "medium": "https://medium.com/@{}",
+    "stackoverflow": "https://stackoverflow.com/users/{}",
+    "twitch": "https://www.twitch.tv/{}",
+    "replit": "https://replit.com/@{}",
 
+    "youtube": "https://www.youtube.com/@{}",
+    "facebook": "https://www.facebook.com/{}",
+    "discord": "https://discord.com/users/{}",
+    "roblox": "https://www.roblox.com/users/profile?username={}",
+    "github_gists": "https://gist.github.com/{}",
+    "gitlab": "https://gitlab.com/{}",
+
+    "hackerrank": "https://www.hackerrank.com/{}",
+    "leetcode": "https://leetcode.com/{}",
+    "codepen": "https://codepen.io/{}",
+    "devto": "https://dev.to/{}",
+
+    "spotify": "https://open.spotify.com/user/{}",
+    "snapchat": "https://www.snapchat.com/add/{}",
+    "patreon": "https://www.patreon.com/{}",
+}
