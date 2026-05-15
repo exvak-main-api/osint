c57a4bf41f01f16d657a55db7e15471806c0c92c
exvak-main-api <sigmagang2025@gmail.com>
2026-05-15 04:53:24 +0100

    Update core.py

--- modules/domain/core.py
+++ modules/domain/core.py
@@ -1,1 +1,12 @@
+from modules.domain.whois import get_whois
+from modules.domain.dns import get_dns
+from modules.domain.email_patterns import guess_patterns
+from modules.domain.website import get_title
 
+def domain_info(domain):
+    return {
+        "whois": get_whois(domain),
+        "dns": get_dns(domain),
+        "email_patterns": guess_patterns(domain),
+        "title": get_title(domain)
+    }
