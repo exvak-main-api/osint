77934684c9a9b4d3949eb7387d8687fe0baf3187
exvak-main-api <sigmagang2025@gmail.com>
2026-05-15 01:35:20 +0100

    Update adobe.py

--- modules/accounts/adobe.py
+++ modules/accounts/adobe.py
@@ -1,25 +1,34 @@
-from lib.colors import *
 from lib.requests import *
 
 async def adobe(target: str):
-    data = {
-        "username": target,
-        "usernameType": "EMAIL"
-    }
-
-    headers = {
-        'x-ims-clientid': 'homepage_milo',
-        'content-type': 'application/json'
-    }
-
-    r = await Request("https://auth.services.adobe.com/signin/v2/users/accounts", headers=headers, json=data).post()
-
     try:
-        if r.json()[0]['authenticationMethods']:
-            print(f"{GREEN}>{WHITE} Adobe")
+        data = {
+            "username": target,
+            "usernameType": "EMAIL"
+        }
 
-        else:
-            print(f"{RED}>{WHITE} Adobe")
+        headers = {
+            'x-ims-clientid': 'homepage_milo',
+            'content-type': 'application/json'
+        }
+
+        r = await Request(
+            "https://auth.services.adobe.com/signin/v2/users/accounts",
+            headers=headers,
+            json=data
+        ).post()
+
+        try:
+            data = r.json()
+
+            if isinstance(data, list) and len(data) > 0:
+                if data[0].get("authenticationMethods"):
+                    return True
+
+            return False
+
+        except:
+            return False
 
     except:
-        print(f"{RED}>{WHITE} Adobe")
+        return False
