e87c8703f5feeaa375d99ebc0c11d0078e7c2461
exvak-main-api <sigmagang2025@gmail.com>
2026-05-15 05:19:57 +0100

    Update sites.py

--- modules/username/sites.py
+++ modules/username/sites.py
@@ -1,6 +1,5 @@
 SITES = {
     "github": "https://github.com/{}",
-    "reddit": "https://www.reddit.com/user/{}",
     "twitter": "https://nitter.net/{}",
     "instagram": "https://www.instagram.com/{}",
     "tiktok": "https://www.tiktok.com/@{}",
@@ -9,20 +8,16 @@ SITES = {
     "stackoverflow": "https://stackoverflow.com/users/{}",
     "twitch": "https://www.twitch.tv/{}",
     "replit": "https://replit.com/@{}",
-
     "youtube": "https://www.youtube.com/@{}",
     "facebook": "https://www.facebook.com/{}",
     "discord": "https://discord.com/users/{}",
     "roblox": "https://www.roblox.com/users/profile?username={}",
     "github_gists": "https://gist.github.com/{}",
     "gitlab": "https://gitlab.com/{}",
-
     "hackerrank": "https://www.hackerrank.com/{}",
     "leetcode": "https://leetcode.com/{}",
     "codepen": "https://codepen.io/{}",
     "devto": "https://dev.to/{}",
-
-    "spotify": "https://open.spotify.com/user/{}",
     "snapchat": "https://www.snapchat.com/add/{}",
     "patreon": "https://www.patreon.com/{}",
 }
