3d50b493339720a15a4b77c50293af0913fd8d3d
exvak-main-api <sigmagang2025@gmail.com>
2026-05-15 01:44:10 +0100

    Update spotify.py

--- modules/accounts/spotify.py
+++ modules/accounts/spotify.py
@@ -1,16 +1,21 @@
 from lib.requests import Request
-from lib.colors import *
 
 async def spotify(target: str):
 
-    r = await Request(f"https://spclient.wg.spotify.com/signup/public/v1/account?validate=1&email={target}").get()
-
     try:
-        if r.json()['status'] == 20:
-            print(f"{GREEN}>{WHITE} Spotify")
+        r = await Request(
+            f"https://spclient.wg.spotify.com/signup/public/v1/account?validate=1&email={target}"
+        ).get()
 
-        else:
-            print(f"{RED}>{WHITE} Spotify")
+        try:
+            data = r.json()
+        except:
+            return False
+
+        if data.get("status") == 20:
+            return True
+
+        return False
 
     except:
-        print(f"{RED}>{WHITE} Spotify")
+        return False
